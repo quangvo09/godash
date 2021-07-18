7b4d46a1ca2588f70ee4813c29dbd0f0267a4b6c
Quang Vo <thanhquang2608@gmail.com>
2021-07-18 19:40:56 +0700

    Update testcase

--- group_by/slice_int_test.go
+++ group_by/slice_int_test.go
@@ -23,6 +23,22 @@ func TestGroupSliceInt(t *testing.T) {
 	group2 := []int{2, 2}
 	res2 := reflect.DeepEqual(output[2], group2)
 	if !res2 {
-		t.Errorf("Group key `1` shouuld be %v", group2)
+		t.Errorf("Group key `2` shouuld be %v", group2)
+	}
+
+	group3 := []int{3, 3}
+	res3 := reflect.DeepEqual(output[3], group3)
+	if !res3 {
+		t.Errorf("Group key `3` shouuld be %v", group3)
+	}
+	group4 := []int{4, 4}
+	res4 := reflect.DeepEqual(output[4], group4)
+	if !res4 {
+		t.Errorf("Group key `4` shouuld be %v", group4)
+	}
+	group5 := []int{5}
+	res5 := reflect.DeepEqual(output[5], group5)
+	if !res5 {
+		t.Errorf("Group key `5` shouuld be %v", group5)
 	}
 }
